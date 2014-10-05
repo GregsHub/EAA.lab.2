e0175b6f197c83746d8e20505a122c10dc7312a6
GregsHub <greg__williams@outlook.com>
2014-10-05 09:10:30 -0700

    Script that accepts Positional Parameters

---
+++ week2/scripts/hw.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#: Title : script.sh
+#: Date : 02-Oct-2014
+#: Author : Greg
+#: Version : 0.1
+#: Description : This is a hellow world script using positional parameters
+#: Options : 
+
+
+printf "%s\n" "Hello, World!" 
+printf "Hello %s $1 nice to meet you \n" 
