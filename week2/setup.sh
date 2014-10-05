e0c6f2a6d318f37e20a0c3f1ec28d8f339ff062a
GregsHub <greg__williams@outlook.com>
2014-10-05 08:41:45 -0700

    Script to create 2 sub directories

---
+++ week2/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#: Title : EAA lab2 Q1
+#: Date : 5 October 2014
+#: Author : Greg Williams
+#: Version : 1.0
+#: Description : scirpt to create 2 sub directories
+#: Options : 
+
+mkdir bin scripts
