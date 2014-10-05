94d6cf6507c488cbeb5f668a4665b52ef255cdee
GregsHub <greg__williams@outlook.com>
2014-10-05 09:14:16 -0700

    script that accepts and uses multiple parameters

---
+++ week2/scripts/data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#: Title : script.sh
+#: Date : 02-Oct-2014
+#: Author : Greg
+#: Version : 0.1
+#: Description : This is a hellow world script using positional parameters
+#: Options : 
+
+
+printf "Hello %s $1 nice to meet you \n I see you favourite colour is %s $3 \n and you were born in %s $2 \n" 
