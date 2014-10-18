bca14ceb777f2afc70e3c4187102c3ea1051e0cb
GregsHub <greg__williams@outlook.com>
2014-10-18 13:00:49 -0700

    Week - testing parameters

---
+++ week4/accept_param.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#: Title : accept_param.sh
+#: Date : 02-Oct-2014
+#: Author : Greg
+#: Version : 0.1
+#: Description : This is a script that accepts and echos parameters 
+#: Options : 
+
+
+printf "Your first word is %s $1 \n"
